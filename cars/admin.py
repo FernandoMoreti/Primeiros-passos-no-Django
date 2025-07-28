de0c92cedaa6a46bb513fcdfe2a95b17a2178c5d
Fernando Moreti <fernandombolela@gmail.com>
2025-07-27 22:00:13 -0300

    Adicionando o CarAdmin para ambiente adiministrativo

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_yaer', 'value')
+    search_filesd = ('model',)
+
+admin.site.register(Car, CarAdmin)
