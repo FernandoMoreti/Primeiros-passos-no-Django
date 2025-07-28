1576d0c9b63ca2474c0e811fe24a719af33ddcff
Fernando Moreti <fernandombolela@gmail.com>
2025-07-27 21:40:06 -0300

    Criando a tabela Cars

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Car(models.Model): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_yaer = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
