b2d32e96bd22db156f2f0dce45bde286a0d25c62
Fernando Moreti <fernandombolela@gmail.com>
2025-07-31 20:27:17 -0300

    Adicionando uma view para entender o conceito

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import carsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', carsView)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
